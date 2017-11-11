5daead6e8adf56b0be2ea8b5476015f259e5bc7c
Yuki-hoge <you@example.com>
2017-11-11 17:52:50 +0900

    Fix (maybe completed)

--- todays-kizunaai.sh
+++ todays-kizunaai.sh
@@ -9,5 +9,5 @@ python search.py >/dev/null 2>&1
 python movemouse.py
 
 # play
-(chromium-browser `cat playlisturl.txt` --start=fullscreen >/dev/null 2>&1)&
+(chromium-browser `cat playlisturl.txt` --window-size=1920,1080 --window-pos=0,0 >/dev/null 2>&1)&
 python close_session_crashed_bubble.py
