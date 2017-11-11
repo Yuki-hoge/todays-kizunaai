aeb72a073c9000adc2364a6cd48892de3aa3ba98
Yuki-hoge <you@example.com>
2017-11-11 15:51:22 +0900

    Fix a little

--- todays-kizunaai.sh
+++ todays-kizunaai.sh
@@ -2,10 +2,10 @@
 python search.py >/dev/null 2>&1
 
 # wake up display
-tvservice -p
-fbset -depth 16
-fbset -depth 32
-xrefresh -d :0.0
+# tvservice -p
+# fbset -depth 16
+# fbset -depth 32
+# xrefresh -d :0.0
 python movemouse.py
 
 # play
